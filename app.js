a39ca29455ed6a1191cdaf9e2d45e32dd0d4a6da
Sam Hamm <sam@codefellows.com>
2019-01-11 17:34:04 -0800

    improved

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 var doggo = document.getElementById("doggo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = slider.value + ' doggo speed!'; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
-  output.innerHTML = this.value + 'doggo speed!';
+  output.innerHTML = this.value + ' doggo speed!';
   var speed = 50 / this.value;
   console.log(speed, 'seconds');
   doggo.style['animation-duration'] = `${speed}s`;
